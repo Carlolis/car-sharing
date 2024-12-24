a78a599b464ef2c48d4eeda04cd2f1e774dca418
Carlolis <cilieff@gmail.com>
2024-12-24 13:23:53 +0100

    wip Remix

--- frontend/app/routes/_auth.login.tsx
+++ frontend/app/routes/_auth.login.tsx
@@ -32,7 +32,7 @@ export default function Login() {
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Conffnexion
+            Connexion
           </h2>
         </div>
         <Form method="post" className="mt-8 space-y-6">
