9ad38cde896cbd1f06c24220fb03fd6362fded5d
Carlolis <cilieff@gmail.com>
2025-01-13 23:14:17 +0100

    configure

--- backend/back/src/scala/Main.scala
+++ backend/back/src/scala/Main.scala
@@ -1,22 +1,24 @@
-import zio.*
-import zio.http.*
-import sttp.tapir.server.ziohttp.{ZioHttpInterpreter, ZioHttpServerOptions}
 import api.TripRoutes
-import services.{TripService}
+import services.TripService
 import sttp.tapir.server.interceptor.cors.CORSConfig.AllowedOrigin
 import sttp.tapir.server.interceptor.cors.{CORSConfig, CORSInterceptor}
+import sttp.tapir.server.ziohttp.{ZioHttpInterpreter, ZioHttpServerOptions}
 import sttp.tapir.ztapir.RIOMonadError
+import zio.*
+import zio.http.*
 
 object Main extends ZIOAppDefault:
-  given RIOMonadError[Any] = new RIOMonadError[Any]
+  given RIOMonadError[Any]               = new RIOMonadError[Any]
   val options: ZioHttpServerOptions[Any] =
-    ZioHttpServerOptions.customiseInterceptors
+    ZioHttpServerOptions
+      .customiseInterceptors
       .exceptionHandler(new DefectHandler())
       .corsInterceptor(
         CORSInterceptor.customOrThrow(
-          CORSConfig.default.copy(
-            allowedOrigin = AllowedOrigin.All
-          )
+          CORSConfig
+            .default.copy(
+              allowedOrigin = AllowedOrigin.All
+            )
         )
       )
       .decodeFailureHandler(CustomDecodeFailureHandler.create())
@@ -25,17 +27,16 @@ object Main extends ZIOAppDefault:
   override def run =
     val port = 8080
     (for
-      _ <- Console.printLine(s"Server starting on http://localhost:$port")
-      _ <- Console.printLine(
-        s"Swagger UI available at http://localhost:$port/docs"
-      )
+
       endpoints <- ZIO.service[TripRoutes]
-      httpApp = ZioHttpInterpreter(options).toHttp(
-        endpoints.endpoints
-      )
-      _ <- Server
-        .install(httpApp)
-      _ <- ZIO.never
+      _         <- ZIO.log(s"Swagger UI available at http://localhost:$port/docs")
+      _         <- ZIO.log(s"Server starting on http://localhost:$port")
+      httpApp    = ZioHttpInterpreter(options).toHttp(
+                     endpoints.endpoints
+                   )
+      _         <- Server
+                     .install(httpApp)
+      _         <- ZIO.never
     yield ()).provide(
       Server.defaultWithPort(port),
       // AuthService.layer,
