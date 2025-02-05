2fbca8770cc7a3cb6833851c4d004d9c0cbecc7d
Carlolis <cilieff@gmail.com>
2025-02-05 18:28:33 +0100

    better ux

--- frontend/app/routes/ia.tsx
+++ frontend/app/routes/ia.tsx
@@ -5,12 +5,19 @@ import { Unexpected } from 'effect/ParseResult'
 import { Ollama } from 'ollama'
 
 import { useEffect, useState } from 'react'
-import { CgSpinner } from 'react-icons/cg'
 import { FiCommand } from 'react-icons/fi'
 import { LuLoaderCircle } from 'react-icons/lu'
+import { MdOutlineGppBad } from 'react-icons/md'
 import { Form, useActionData } from 'react-router'
 import { ChatChunk, streamResponse } from '~/contexts/ia.util'
 import { Remix } from '~/runtime/Remix'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '../components/ui/select'
 
 export const action = Remix.action(
   T.gen(function* () {
@@ -78,12 +85,44 @@ export default function IA() {
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-white">
-            Demandez à l'IA
+            Demandez à l&apos;IA
           </h2>
         </div>
         <Form className="mt-8 space-y-6" method="post">
           <div className="rounded-md shadow-sm -space-y-px">
             <div className="flex space-x-2">
+              <div className="flex-1">
+                <Select>
+                  <SelectTrigger
+                    id="model"
+                    name="model"
+                  >
+                    <SelectValue placeholder="Choisi un modèle" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="codestral:latest">🇫🇷 Mistral Codestral Latest</SelectItem>
+                    <SelectItem value="mistral-small:24b">🇫🇷 Mistral Small 3 24B</SelectItem>
+                    <SelectItem value="deepseek-coder-v2:latest">
+                      <div>
+                        <MdOutlineGppBad className="inline-block mr-1 text-red-600" />
+                        🇨🇳 DeepSeek Coder V2 Latest
+                      </div>
+                    </SelectItem>
+                    <SelectItem value="deepseek-r1:32b-qwen-distill-q4_K_M">
+                      <MdOutlineGppBad className="inline-block mr-1 text-red-600" />
+                      🇨🇳 DeepSeek R1 32B Distill
+                    </SelectItem>
+                    <SelectItem value="deepseek-r1:14b-qwen-distill-q4_K_M">
+                      <MdOutlineGppBad className="inline-block mr-1 text-red-600" />
+                      🇨🇳 DeepSeek R1 14B Distill
+                    </SelectItem>
+                    <SelectItem value="deepseek-r1:latest">
+                      <MdOutlineGppBad className="inline-block mr-1 text-red-600" />
+                      🇨🇳 DeepSeek R1 Latest
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
               <div className="flex-1">
                 <label htmlFor="message" className="sr-only">
                   Message
@@ -93,31 +132,9 @@ export default function IA() {
                   name="message"
                   type="text"
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 dark:border-gray-600 placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white bg-white dark:bg-gray-800 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 dark:border-gray-600 placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white bg-white dark:bg-gray-800 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Demandez à l'IA"
                 />
-              </div>
-              <div className="flex-1">
-                <label htmlFor="model" className="sr-only">
-                  Modèle
-                </label>
-                <select
-                  id="model"
-                  name="model"
-                  required
-                  className=" rounded-none relative block w-full px-3 py-2 border border-gray-300 dark:border-gray-600 placeholder-gray-500 dark:placeholder-gray-400 text-gray-900 dark:text-white bg-white dark:bg-gray-800 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm cursor-pointer"
-                >
-                  <option value="codestral:latest">Codestral Latest</option>
-                  <option value="mistral-small:24b">Mistral Small 24B</option>
-                  <option value="deepseek-coder-v2:latest">DeepSeek Coder V2 Latest</option>
-                  <option value="deepseek-r1:32b-qwen-distill-q4_K_M">
-                    DeepSeek R1 32B Qwen Distill Q4 K M
-                  </option>
-                  <option value="deepseek-r1:14b-qwen-distill-q4_K_M">
-                    DeepSeek R1 14B Qwen Distill Q4 K M
-                  </option>
-                  <option value="deepseek-r1:latest">DeepSeek R1 Latest</option>
-                </select>
               </div>
             </div>
           </div>
@@ -137,7 +154,6 @@ export default function IA() {
           </div>
         </Form>
         <div className="mt-8 text-center">
-          <h1 className="text-2xl font-bold text-gray-900 dark:text-white">Réponse</h1>
           {isLoading ?
             <LuLoaderCircle className="mx-auto my-4 text-indigo-600 animate-spin" size={48} /> :
             <FiCommand className="mx-auto my-4 text-indigo-600" size={48} />}
@@ -146,6 +162,13 @@ export default function IA() {
               {texte}
             </div>
           )}
+        </div>
+        <div className="mt-8 text-center">
+          <MdOutlineGppBad className="mx-auto my-4 text-red-600" size={24} />
+          <p className="text-sm text-gray-600 dark:text-gray-400">
+            Les modèles DeepSeek peuvent être censurés. Exemple: les événements de la place
+            Tiananmen en 1989.
+          </p>
         </div>
       </div>
     </div>
